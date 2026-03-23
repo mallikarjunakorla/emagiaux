09ab0f8bc21dbf794558a42ca0f42d0e4b7cc707
Mallikarjuna <arjunkorla@gmail.com>
2026-03-23 11:40:41 +0530

    fix: add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: process.env.VITE_BASE_PATH || '/',
+  base: process.env.VITE_BASE_PATH || '/emagiaux/',
 
   resolve: {
     alias: {
